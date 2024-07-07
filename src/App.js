99a092443c517827e9826512b54383285e5d5071
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-07-07 18:03:39 +0200

    loaded questions from a fake api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import Header from "./Header.js";
 import Main from "./Main.js";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "Error" };
+
+    default:
+      throw new Error("Action is unknown ");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:9000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
